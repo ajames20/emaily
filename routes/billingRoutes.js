ccf61b62f398b8a6d84baa32931b94faf519c9e8
Andrew James <ajames20@gmail.com>
2017-09-10 15:02:01 +0300

    edit stripe

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,5 +1,5 @@
 const keys = require('../config/keys');
-const stripe = require('stripe')(keys.stripeSecretKey);
+const stripe = require('stripe')(keys.stripePublishableKey);
 const requireLogin = require('../middleware/requierLogin');
 
 module.exports = app => {
